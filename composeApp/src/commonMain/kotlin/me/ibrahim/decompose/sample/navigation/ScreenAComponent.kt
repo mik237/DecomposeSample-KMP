45a73a7b380dcb0c3805b58d32b2d8cf3363b2a9
muhammadibrahim <mik237@gmail.com>
2025-02-04 22:24:09 +0500

    RootComponent added

---
+++ composeApp/src/commonMain/kotlin/me/ibrahim/decompose/sample/navigation/ScreenAComponent.kt
@@ -0,0 +1,8 @@
+package me.ibrahim.decompose.sample.navigation
+
+import com.arkivanov.decompose.ComponentContext
+
+class ScreenAComponent(
+    componentContext: ComponentContext
+) : ComponentContext by componentContext {
+}
